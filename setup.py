d88b4dbef08db142942ca3c379c6661cbb3da617
T. Shaffner <tom.shaffner@yahoo.com>
2021-02-01 20:32:14 -0500

    pypi deployment testing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pithermalcam",
-    version="0.1.1",
+    version="0.1.2",
     author="Tom Shaffner",
     author_email="tom.shaffner@yahoo.com",
     description="A package which connects an MLX90640 Thermal IR Camera to a Raspberry Pi for viewing or web streaming.",
